ece7c7b1d2ec5fa277b9bb3b07a4a9fc6c7db483
assepas <108719859+assepas@users.noreply.github.com>
2022-11-23 15:24:44 +0100

    toch nog 1 keer proberen

--- Packages/Netherlands3D/Core/Runtime/Scripts/TileSystem/BinaryMeshLayer.cs
+++ Packages/Netherlands3D/Core/Runtime/Scripts/TileSystem/BinaryMeshLayer.cs
@@ -90,6 +90,8 @@ namespace Netherlands3D.TileSystem
 			if (Datasets[lod].path.StartsWith("https://") || Datasets[lod].path.StartsWith("file://"))
 			{
 				url = Datasets[lod].path;
+			} else {
+				url = System.IO.Path.Combine(Application.streamingAssetsPath, Datasets[lod].path);
 			}
 
 			url = url.ReplaceXY(tileChange.X, tileChange.Y);
